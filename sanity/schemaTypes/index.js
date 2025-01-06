7551166332cd0f9a0e42db8d4dcb58eab967b46b
Festus <techarenaz01@gmail.com>
2025-01-06 12:08:52 -0800

    added the schema types for sanity io cms

--- sanity/schemaTypes/index.js
+++ sanity/schemaTypes/index.js
@@ -1,3 +1,7 @@
+import { authors } from "./authors";
+import { categories } from "./categories";
+import { projects } from "./projects";
+
 export const schema = {
-  types: [],
+  types: [authors, categories, projects],
 }
