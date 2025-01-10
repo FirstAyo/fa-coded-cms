3f99088adcd893bb1fccf082a9001ee99fe3b5d8
Festus <techarenaz01@gmail.com>
2025-01-09 22:21:56 -0800

    changed the search button to select option

--- app/components/SearchBox.jsx
+++ app/components/SearchBox.jsx
@@ -1,20 +1,29 @@
 import React from "react";
 
 function SearchBox() {
-
   return (
     <>
       <section>
-        <div className="flex items-center justify-center my-10 w-[60%] mx-auto">
+        <div className="flex items-center justify-center my-10 w-[40%] mx-auto">
           <input
-            placeholder="Search Blog"
+            placeholder="Select from Categories"
             className="border py-2 px-4 rounded-l-md md:w-[60%]"
           />
-          <button className="bg-gradient-to-r from-purple-700 via-orange-700 to-red-800 text-white px-5 py-2 border rounded-r-md md:w-[20%]">
+          {/* <button className="bg-gradient-to-r from-purple-700 via-orange-700 to-red-800 text-white px-5 py-2 border rounded-r-md md:w-[20%]">
             Search
-          </button>
+          </button> */}
+          <select
+            name="categories"
+            id="categories"
+            className="uppercase font-semibold border-2 rounded-r-md py-2.5 px-4 bg-red-700/80 text-white"
+          >
+            <option value="volvo">All Projects</option>
+            <option value="volvo">HTML & CSS</option>
+            <option value="saab">REACT JS</option>
+            <option value="opel">JavaScript</option>
+            <option value="audi">Tailwind CSS</option>
+          </select>
         </div>
-
       </section>
     </>
   );
