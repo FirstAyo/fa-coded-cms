5b7632bb067edb4c4c0cea5e99d347f1184f23e6
Festus <techarenaz01@gmail.com>
2025-01-09 22:54:36 -0800

    added border to the search component

--- app/components/SearchBox.jsx
+++ app/components/SearchBox.jsx
@@ -4,10 +4,10 @@ function SearchBox() {
   return (
     <>
       <section>
-        <div className="flex items-center justify-center my-10 lg:w-[50%] mx-auto">
+        <div className="flex items-center justify-center flex-col gap-3 md:gap-0 md:flex-row my-10 w-[95%] lg:w-[50%] mx-auto">
           <input
             placeholder="Select from Categories"
-            className="border py-2 px-4 rounded-l-md md:w-[60%]"
+            className="border border-red-700 py-2 px-4 rounded-l-md w-[100%] md:w-[60%]"
           />
           {/* <button className="bg-gradient-to-r from-purple-700 via-orange-700 to-red-800 text-white px-5 py-2 border rounded-r-md md:w-[20%]">
             Search
@@ -15,7 +15,7 @@ function SearchBox() {
           <select
             name="categories"
             id="categories"
-            className="uppercase font-semibold border-2 rounded-r-md py-2.5 px-4 bg-red-700/80 text-white"
+            className="uppercase font-semibold border border-red-700 rounded-r-md py-2.5 px-4 bg-red-700/80 text-white"
           >
             <option value="volvo">All Projects</option>
             <option value="volvo">HTML & CSS</option>
