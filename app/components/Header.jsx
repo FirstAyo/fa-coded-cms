ad11514ad1d024e0848ba213a62bb3399a0f3e57
Festus <techarenaz01@gmail.com>
2025-01-12 21:50:51 -0800

    made some changes to the px and width of the tags in the header components

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -12,7 +12,7 @@ function Header() {
 
   const menuItems = [
     { name: "Home", href: "/" },
-    { name: "My Projects", href: "/project" },
+    { name: "Projects", href: "/project" },
     { name: "About", href: "/about" },
     { name: "Contact Me", href: "/contact" },
   ];
@@ -23,14 +23,14 @@ function Header() {
           <nav className="flex items-center justify-between w-[95%] mx-auto py-3">
             <Link
               href="/"
-              className="flex-1 text-2xl md:text-3xl font-extrabold text-red-700"
+              className="lg:w-[30%] text-2xl md:text-3xl font-extrabold text-red-700"
             >
               FA - <span className="">Coded</span>
             </Link>
 
             <div
               className={
-                "lg:flex absolute top-16 w-full bg-red-700 lg:bg-transparent mx-auto lg:top-0 left-0 right-0 lg:relative flex-1 py-2 items-center justify-center lg:justify-between lg:show lg:gap-0 " +
+                "lg:flex absolute top-16 bg-red-700 lg:bg-transparent mx-auto lg:top-0 left-0 right-0 lg:relative w-lg:[40%] py-2 items-center justify-center lg:show " +
                 (toggleMenu ? "flex show" : "hidden")
               }
             >
@@ -38,7 +38,7 @@ function Header() {
                 <div key={index} className="py-1">
                   <Link
                     href={menuItem.href}
-                    className="text-lg px-3 font-bold hover:bg-red-700 hover:text-white py-4 text-white lg:text-red-700"
+                    className="text-lg px-2 md:px-7 font-bold hover:bg-red-700 hover:text-white py-3 text-white lg:text-red-700"
                     onClick={() => setToggleMenu((currentVal) => !currentVal)}
                   >
                     {" "}
@@ -48,7 +48,7 @@ function Header() {
               ))}
             </div>
 
-            <div className="flex-1 flex justify-end">
+            <div className="lg:w-[30%] flex justify-end">
               <Link
                 href="https://drive.google.com/file/d/19MNlkOHemlPxuyC5jc75x8CvhGK6rxuj/view?usp=sharing"
                 target="_blank"
